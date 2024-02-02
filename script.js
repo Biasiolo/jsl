e48811dbd1f1f34110eed4ba99eae460d546c230
Biasiolo <biasiolorafael@gmail.com>
2024-02-01 21:39:58 -0300

    ajustes

--- script.js
+++ script.js
@@ -24,3 +24,21 @@ window.addEventListener('scroll', function() {
     imgElement.style.transform = 'scale(' + zoomLevel + ')';
 });
 
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+    var imgElement = document.getElementById('animatedImage2');
+    
+    // Ajuste esses valores conforme necessário
+    var zoomFactor = 0.5;
+    var maxZoomOut = 0.8;
+
+    // Calcule o zoom-out com base no scroll
+    var zoomLevel = 1.3 - (scrollPosition * zoomFactor / window.innerHeight);
+
+    // Limite o zoom-out para evitar que a imagem fique muito pequena
+    zoomLevel = Math.max(zoomLevel, maxZoomOut);
+
+    // Aplique a transformação de zoom-out
+    imgElement.style.transform = 'scale(' + zoomLevel + ')';
+});
+
