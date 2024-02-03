a8ba28d1bc0b514d60d24f5f6487a79e02b1f7c6
Biasiolo <biasiolorafael@gmail.com>
2024-02-02 23:38:40 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// função para abri pdf
 document.addEventListener('DOMContentLoaded', function () {
     // Adiciona um ouvinte de evento ao botão para mostrar/ocultar o PDF quando clicado
     document.getElementById('btnAbrirPDF').addEventListener('click', function () {
@@ -6,16 +7,26 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+//função botão whatsapp para capturar dados
+function enviarWhatsApp() {
+    var nome = document.getElementById("nome").value;
+    var email = document.getElementById("email").value;
+    var mensagem = document.getElementById("mensagem").value;
+    var linkWhatsApp = "https://api.whatsapp.com/send?phone=+5511985888974&text=Olá, gostaria de mais informações. Meu nome é " + encodeURIComponent(nome) + ". Meu email é " + encodeURIComponent(email) + ". " + "Mensagem: " + encodeURIComponent(mensagem) + ". ";
+    window.open(linkWhatsApp, '_blank');
+}
+
+//função animação de imagens
 window.addEventListener('scroll', function() {
     var scrollPosition = window.scrollY;
     var imgElement = document.getElementById('animatedImage');
     
     // Ajuste esses valores conforme necessário
     var zoomFactor = 0.5;
-    var maxZoomOut = 0.8;
+    var maxZoomOut = 0.7;
 
     // Calcule o zoom-out com base no scroll
-    var zoomLevel = 1 - (scrollPosition * zoomFactor / window.innerHeight);
+    var zoomLevel = 1.05 - (scrollPosition * zoomFactor / window.innerHeight);
 
     // Limite o zoom-out para evitar que a imagem fique muito pequena
     zoomLevel = Math.max(zoomLevel, maxZoomOut);
